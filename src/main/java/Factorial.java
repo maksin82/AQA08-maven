00f07cdf7a8fafc97c132b74dbe739cb9853d094
maksin <maksin82@gmail.com>
2024-07-23 13:45:40 +0300

    Factorial

---
+++ src/main/java/Factorial.java
@@ -0,0 +1,9 @@
+public class Factorial {
+    static int calcFactorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
